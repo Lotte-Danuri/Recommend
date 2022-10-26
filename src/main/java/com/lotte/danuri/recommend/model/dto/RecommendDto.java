a1cfa3147d9a33f93dcde9d70848fefa647a383e
jinwooooooo <ko440338@gmail.com>
2022-10-26 14:33:23 +0900

    (FEAN-193) feat : mongodb documment 생성을 위한 RecommendDto

--- src/main/java/com/lotte/danuri/recommend/model/dto/RecommendDto.java
+++ src/main/java/com/lotte/danuri/recommend/model/dto/RecommendDto.java
@@ -4,9 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Document
 @NoArgsConstructor
@@ -17,7 +21,7 @@ public class RecommendDto {
 
     @Field("_id")
     @Id
-    private Long id;
+    private ObjectId id;
 
     @Field("user_id")
     private Long memberId;
@@ -26,5 +30,10 @@ public class RecommendDto {
     private Long productId;
 
     @Field("preference")
-    private Long clickCount;
+    private Double clickCount;
+
+
+    @Field("created_at")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime createDate;
 }
