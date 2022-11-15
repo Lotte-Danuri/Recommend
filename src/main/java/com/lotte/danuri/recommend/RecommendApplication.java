9811a1ed8846b2d060fe0d6e1ed9e1fa8b7d6d19
jinwooooooo <54875278+jinwooooooo@users.noreply.github.com>
2022-11-15 11:35:34 +0900

    (develop) feat : 서버 시간 동기화

--- src/main/java/com/lotte/danuri/recommend/RecommendApplication.java
+++ src/main/java/com/lotte/danuri/recommend/RecommendApplication.java
@@ -6,12 +6,19 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableAsync
 public class RecommendApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RecommendApplication.class, args);
 	}
